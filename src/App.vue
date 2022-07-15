6a35e5a99cb9f9c0c84cbe4415d75eb8d69b4b2d
Arbalest <499624095@qq.com>
2022-07-15 13:42:50 +0800

    feat: set antdv locale options

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <template>
-    <router-view />
+    <a-config-provider :locale="locale">
+        <router-view />
+    </a-config-provider>
 </template>
 
 <script lang="ts">
+import zhCN from 'ant-design-vue/es/locale/zh_CN'
 export default defineComponent({
-    name: 'App'
+    name: 'App',
+    data() {
+        return {
+            locale: zhCN
+        }
+    }
 })
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="less" scoped></style>
